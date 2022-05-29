34e3f72c28c10b0a7b5c6bc5309a2b02e653d278
rathlucas <lucin189@gmail.com>
2022-05-29 16:15:21 -0300

    feat: Add states and ref for mount on app opening

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,40 @@
+import { useState, useEffect, useRef } from 'react';
+
 import styled from 'styled-components';
+
 import narutoImg from '../images/naruto.png';
+
 import { Quotes } from '../components/quotes/Quotes';
+import { getQuote } from '../services/quotesService/quotesService';
 
 export default function App() {
+  const isMounted = useRef(true);
+
+  console.log(isMounted);
+
+  const [quoteState, setQuoteState] = useState({
+    quote: 'test',
+    speaker: 'speaker',
+  });
+
+  const onUpdate = async () => {
+    if (isMounted) {
+      const quote = await getQuote();
+      setQuoteState(quote);
+    }
+  };
+
+  useEffect(() => {
+    onUpdate();
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
   return (
     <Content>
-      <Quotes quote={'test'} speaker={'test speaker'} />
+      <Quotes {...quoteState} onUpdate={onUpdate} />
       <NarutoImg src={narutoImg} alt="Naruto holding a kunai" />
     </Content>
   );
